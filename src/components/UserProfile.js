89f3d023e3d26d8df54b7c042a9d18286e694723
RAKESHRAJ MAHAKUD <53381380+rakeshrajmahakud@users.noreply.github.com>
2022-08-07 09:44:25 +0530

    Update UserProfile.js

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -3,7 +3,12 @@ import React from 'react'
 import { UserContext } from "./App"
 
 const UserProfile = () =>{
-
+  <UserContext.Consumer>
+    {data => <>
+     <h1 id="name">{data.name}</h1>
+     <h1 id="age">{data.age}</h1>
+    }
+  </UserContext.Consumer>
 }
 
 export {UserProfile}
