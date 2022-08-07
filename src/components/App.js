f4364cb8eb9a51776c8f5d67956fe01ae1a14a75
RAKESHRAJ MAHAKUD <53381380+rakeshrajmahakud@users.noreply.github.com>
2022-08-07 09:34:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, {createContext} from 'react'
 import '../styles/App.css';
 import { UserProfile } from './UserProfile';
+export const contextData = createContext();
+
 
 
 const App = () => {
 
   return (
     <div id="main">
-      
+      <contextData.Provider value={{name:"Newton",age:3}}>
         <UserProfile />
+      </contextData.Provider>
       
     </div>
   )
